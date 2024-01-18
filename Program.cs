1b97899f636bdf18b5c5e11fb1c6de390311b031
Michelle Mabuyo <michelle@apollographql.com>
2024-01-18 15:32:37 -0500

    lesson 5: cors for sandbox

--- Program.cs
+++ Program.cs
@@ -4,7 +4,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGraphQLServer().AddQueryType<Query>();
 
+builder
+    .Services
+    .AddCors(options =>
+    {
+        options.AddDefaultPolicy(builder =>
+        {
+            builder
+                .WithOrigins("https://studio.apollographql.com")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+    });
+
 var app = builder.Build();
+
+app.UseCors();
 
 app.MapGraphQL();
 
