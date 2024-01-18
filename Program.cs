a39dca16fd489730d0320b1e9e15c8beff278608
Michelle Mabuyo <michelle@apollographql.com>
2024-01-18 15:27:28 -0500

    lesson 3: installing Hot Chocolate, server setup

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGraphQLServer();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGraphQL();
 
 app.Run();
