d0a5a6f11aba7d85732eed70188fc6b57764469f
Michelle Mabuyo <michelle@apollographql.com>
2024-11-21 13:51:43 -0500

    lesson 7: query entry point

--- Types/Query.cs
+++ Types/Query.cs
@@ -2,8 +2,14 @@ namespace Odyssey.IntroHCListings.Types;
 
 public class Query
 {
-    public string Hello()
+    [GraphQLDescription("A curated array of listings to feature on the homepage")]
+    public List<Listing> FeaturedListings()
     {
-        return "Hello world";
+        return new List<Listing>
+        {
+            new Listing("1", "The A-Frame in Mraza"),
+            new Listing("2", "Qo'noS Mountaintop Cabin"),
+            new Listing("3", "Interstellar cottage in Vaperi III")
+        };
     }
 }
