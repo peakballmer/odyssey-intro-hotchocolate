d920ad91c54626e62ffa165e6708041688d2d159
Michelle Mabuyo <michelle@apollographql.com>
2024-11-22 17:20:12 -0500

    lesson 12: resolver chain

--- Types/Listing.cs
+++ Types/Listing.cs
@@ -22,8 +22,40 @@ public class Listing
     [GraphQLDescription("The listing's description")]
     public string Description { get; set; }
 
+    private bool hasNullAmenityProperties()
+    {
+        if (_amenities == null)
+        {
+            return true;
+        }
+
+        // an array of booleans - true if the amenity contains a null property
+        var hasNullProperties = _amenities.Select(
+            amenity =>
+                amenity
+                    .GetType()
+                    .GetProperties()
+                    .Any(propertyInfo => propertyInfo.GetValue(amenity) == null)
+        );
+
+        return hasNullProperties.Any(a => a);
+    }
+
     [GraphQLDescription("The amenities available for this listing")]
-    public List<Amenity> Amenities { get; set; }
+    public async Task<List<Amenity>> Amenities(ListingsDataSource.ListingsService listingsService)
+    {
+        if (_amenities == null || hasNullAmenityProperties())
+        {
+            var response = await listingsService.GetListingAmenitiesAsync(this.Id);
+            return response.Select(amenity => new Amenity(amenity)).ToList();
+        }
+        else
+        {
+            return _amenities;
+        }
+    }
+
+    private List<Amenity>? _amenities;
 
     public Listing(string id, string title, string description, List<Amenity>? amenities)
     {
@@ -31,7 +63,7 @@ public class Listing
         Title = title;
         Description = description;
 
-        Amenities = amenities;
+        _amenities = amenities;
     }
 
     public Listing(ListingsDataSource.Listing obj)
@@ -43,6 +75,6 @@ public class Listing
         ClosedForBookings = obj.ClosedForBookings;
         Description = obj.Description;
 
-        Amenities = obj.Amenities.Select(amenity => new Amenity(amenity)).ToList();
+        _amenities = obj.Amenities.Select(amenity => new Amenity(amenity)).ToList();
     }
 }
