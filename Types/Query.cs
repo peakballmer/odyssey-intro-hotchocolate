9540ceb7bef3b818e53eae693b9eed5eaaae0a2e
claes <claeswdev@gmail.com>
2025-04-10 17:04:41 +0200

    feat: replace Hello query with FeaturedPlaylists resolver to return curated playlists

--- Types/Query.cs
+++ Types/Query.cs
@@ -2,9 +2,14 @@ namespace Odyssey.MusicMatcher;
 
 public class Query
 {
-    // Hello() is an example of a query resolver.
-    public string Hello()
+    [GraphQLDescription("Playlists hand-picked to be featured to all users.")]
+    public List<Playlist> FeaturedPlaylists()
     {
-        return "Hello world";
+        return new List<Playlist>
+        {
+            new Playlist("1", "GraphQL Groovin"),
+            new Playlist("2", "Graph Explorer Jams"),
+            new Playlist("3", "Interpretive GraphQL Dance")
+        };
     }
 }
